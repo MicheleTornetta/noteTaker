15ef19ce2b2e6341f9de013b9059728dd0149da0
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2022-11-19 12:05:54 -0500

    post & save function

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const app = express()
 const port = 3000
 const path = require('path');
+const bodyParser = require('body-parser');
 const { readNotes, saveNotes } = require('./src/db-control.js');
+
+const jsonParser = bodyParser.json();
 
 //tell to serve up public files in the public folder
 app.use(express.static('public', {
@@ -23,21 +26,44 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:noteId', (req, res) => {
-  const id = req.params.noteId;
+  const id = Number(req.params.noteId);
 
   const notes = readNotes();
 
   for (let i = 0; i < notes.length; i++) {
     if (notes[i].id === id) {
       notes.splice(i, 1);
+      saveNotes(notes);
+      
       res.status(200).send('success!');
 
-      saveNotes(notes);
       return;
     }
   }
 
   res.status(400).send('note not found ;(');
+});
+
+app.post('/api/notes', jsonParser, (req, res) => {
+  const notes = readNotes();
+  
+  let biggestId = -9999999999;
+
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id > biggestId) {
+      biggestId = notes[i].id;
+    }
+  }
+
+  notes.push({
+    title: req.body.title,
+    text: req.body.text,
+    id: biggestId + 1
+  });
+
+  saveNotes(notes);
+
+  res.status(200).send('OK');
 });
 
 // fs.writeFile("db/filename.json", JSON.stringify(reviews), (err) => {
